c66339e2ae1534b537d5b0ba3c01b95a83f1bbda
Shania Lopez <slopez@fandm.edu>
2023-03-25 22:33:07 -0400

    tentative finish

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const p1 = document.createElement("p");
-p1.textContent = "Hi! My name is Shania Lopez and currently I'm a preschool teacher. I'm thrilled to make a career change into software engineering. I graduated from Franklin & Marshall College in 2021 with an art degree I absolutely do not use! Rediscovering a passion for code has lead me to Flatiron School and I cannot wait to see where this profession journey takes me.";
+p1.textContent = "Hi! My name is Shania Lopez and currently I'm a preschool teacher. I'm thrilled to make a career change into software engineering. I graduated from Franklin & Marshall College in 2021 with an art degree I absolutely do not use! Rediscovering a passion for code has lead me to Flatiron School and I cannot wait to see where this journey takes me.";
 document.querySelector("#body").appendChild(p1);
 
 document.getElementById("about").onclick = function() {myFunction()};
