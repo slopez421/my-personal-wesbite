3c4cf6067074c887ff474338ff4fb7c09b4d1d69
Shania Lopez <slopez@fandm.edu>
2023-03-25 21:00:35 -0400

    added text with js, a click event, and hover colors for the links with css

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const p1 = document.createElement("p");
-p1.textContent = "This content added by JavaScript";
+p1.textContent = "Hi! My name is Shania Lopez and currently I'm a preschool teacher. I'm thrilled to make a career change into software engineering. I graduated from Franklin & Marshall College in 2021 with a degree I absolutely do not use! Rediscovering a passion for code has lead me to Flatiron School and I cannot wait to see where this profession journey takes me.";
 document.querySelector("#body").appendChild(p1);
 
+document.getElementById("about").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("about").innerHTML = "About Me";
+}
